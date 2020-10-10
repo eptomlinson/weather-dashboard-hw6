044aa8323746df5c3f0dfdaa8721589f5503709c
Erik Tomlinson <eptomlinson@gmail.com>
2020-10-10 15:20:06 -0500

    added ajax to function for 5 day forecast

--- script.js
+++ script.js
@@ -6,22 +6,39 @@
 var weatherApiKey = "883f353f6e06551f30c9aba3bab1545f";
 function getWeather(city) {
     console.log(city);
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + weatherApiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + weatherApiKey + "&units=imperial";
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=" + weatherApiKey + "&units=imperial";
+
     $.ajax({
     url:queryURL,
     method:"GET"
     }).then(function(response) {
         console.log(response);
+        $("#currentTemp").text(response.main.temp)
+    })
+
+    $.ajax({
+        url:fiveDayURL,
+        method: "GET"
+    }).then(function(fiveDayRes){
+        console.log(fiveDayRes);
+        for(i=0; i<fiveDayRes.list.length; i+=8){
+            console.log(fiveDayRes.list[i].main.temp)
+            //0,8,16,24,32
+            $("#index-"+i).text(fiveDayRes.list[i].main.temp)
+        }
     })
 }
 $("#search-button").on("click", function() {
-    var city = $("#city-input").val();
+    var city = $("#search-value").val();
     console.log(city);
     getWeather(city);
-    var weatherURL = api.openweathermap.org/data/2.5/weather?q={city}&appid={883f353f6e06551f30c9aba3bab1545f}
-        ;
-}
-    $.ajax({
-        url: 
-    })
-    })
+    // var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=883f353f6e06551f30c9aba3bab1545f&units=imperial`
+    //     $.ajax({
+    //         url: weatherURL,
+    //         method: "GET"
+    //     }).then(function(res){
+    //         console.log(res);
+    //     })
+    });
+    
